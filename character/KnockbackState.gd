026bcb67bb160891e33858273e9c51bbccf77cd3
chickensalad303 <miloiversenginsburg@gmail.com>
2024-04-19 22:50:39 +0200

    made get_hit shader, which gets put into animationplayer, so it's just part of the anim pipeline now

--- character/KnockbackState.gd
+++ character/KnockbackState.gd
@@ -5,11 +5,12 @@ extends State
 @export var knockback_weight : float = 0.02
 var knockback_to_apply : Vector2
 
+@export var get_hit_anim_name : String = "get_hit"
 func enter():
 	if !character_body:
 		push_error("please assign all the exports, they neeeeeeeeeeded to worki")
 		return
-	
+	playback.travel(get_hit_anim_name)
 	print("Help iva bean noonoked bucks  -  params -  ", parameters)
 	character_body.speed = 0
 	character_body.direction = Vector2.ZERO
@@ -22,6 +23,8 @@ func physics_update(delta : float):
 func activate_knockback(knockback_limit: float):
 	# length of vector2 is just pythagoras theorem, good way to compare 2 vectors
 	if (knockback_to_apply.length() > knockback_limit):
+		# instead of below, do it in animationplayer, more efficient
+		#get_node("../../Sprite2D").get_material().set_shader_parameter("is_active", true)
 		knockback_to_apply = lerp(knockback_to_apply, Vector2.ZERO, knockback_weight)
 		#print(knockback_to_apply.length(), " > ", 50)
 	else:
